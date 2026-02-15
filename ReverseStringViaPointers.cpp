216b9c7ba1e164a0138e3db293d0599f33f4ad77
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-15 05:54:37 +0300

    Implement string reversal using pointers

--- ReverseStringViaPointers.cpp
+++ ReverseStringViaPointers.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    
+    char *startPtr;
+    char temp;
+    char *endPtr;
+    char str[] = "Test";
+
+    startPtr = str;
+    endPtr = str + strlen(str) - 1;
+
+    for (; endPtr >= startPtr; endPtr--, startPtr++)
+    {
+        temp = *startPtr;
+        *startPtr = *endPtr;
+        *endPtr = temp;
+    }
+
+    cout << str;
 }
